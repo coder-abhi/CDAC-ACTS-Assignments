a8300e1822076ae76c497a4654e1ac4fa9978a48
coder-abhi <abhi.kamble.3987@gmail.com>
2022-10-10 01:12:19 +0530

    GIT IGNORE SUCCED

--- src/PrimeNo.java
+++
@@ -1,6 +0,0 @@
-package com.cdac.acts;
-public class PrimeNo{
-	public static void PrintPrimeNo() {
-		System.out.println("This are the prime Nos");
-	}
-}
--- src/TesterPrimeNo.java
+++
@@ -1,6 +0,0 @@
-import com.cdac.acts.PrimeNo;
-class TesterPrimeNo {
-	public static void main(String... args) {
-		PrimeNo.PrintPrimeNo();
-}
-}
